cd5830e9844334557fbec3efe775e563d9b41cb8
pravinc3798 <pravin.choudhary88@gmail.com>
2022-08-29 21:50:54 +0530

    Added BinarySearchTree class

---
+++ BinarySearchTree.cs
@@ -0,0 +1,78 @@
+﻿namespace HashTable_BinarySearchTree
+{
+    public class BinarySearchTree
+    {
+        private class Node
+        {
+            public int Value;
+            public Node Right;
+            public Node Left;
+
+            public Node(int value)
+            {
+                Value = value;
+            }
+        }
+
+        private Node Root;
+
+        public void Add(int value)
+        {
+            Root = InsertRecursively(Root, value);
+        }
+
+        private Node InsertRecursively(Node currentNode, int value)
+        {
+            if (currentNode == null)
+                currentNode = new Node(value);
+
+            else if (value > currentNode.Value)
+                currentNode.Right = InsertRecursively(currentNode.Right, value);
+
+            else
+                currentNode.Left = InsertRecursively(currentNode.Left, value);
+
+            return currentNode;
+        }
+
+        public void Search(int value)
+        {
+            SearchRecursively(Root, value);
+        }
+
+        private void SearchRecursively(Node currentNode, int value)
+        {
+            if (currentNode == null)
+                Console.WriteLine(" '{0}' Not found in the Tree",value);
+
+            else if (currentNode.Value == value)
+                Console.WriteLine(" '{0}' Present in the Tree",value);
+
+            else if (value > currentNode.Value)
+                SearchRecursively(currentNode.Right, value);
+
+            else
+                SearchRecursively(currentNode.Left, value);
+        }
+
+        public override string ToString()
+        {
+            return Print(Root, 0);
+        }
+
+        private string Print(Node currentNode, int l)
+        {
+            // copied from google and not able to understand the logic as of today (20/8/22)
+
+            string res = (currentNode == null) ? "NULL" : currentNode.Value.ToString();
+            res = $"{"".PadRight(1, '-')}{res}\n";
+
+            if (currentNode == null) return res;
+
+            res += $"{Print(currentNode.Left, l + 1)}";
+            res += $"{Print(currentNode.Right, l + 1)}";
+
+            return res;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,62 @@
+﻿namespace HashTable_BinarySearchTree
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("----------------------------HASH TABLE---------------------------------------");
+
+            var UC1text = "To be or not to be";
+            var UC2text = "Paranoids are not paranoid because they are paranoid but because they keep putting themselves deliberately into paranoid avoidable situations";
+
+            var UC1Array = UC1text.Split(' ');
+            var UC2Array = UC2text.Split(' ');
+
+            var hTable1 = new StringCountHashTable(UC1Array.Length);
+            var hTable2 = new StringCountHashTable(UC2Array.Length);
+
+            foreach (var word in UC1Array)
+                hTable1.Add(word);
+
+            foreach (var word in UC2Array)
+                hTable2.Add(word);
+
+            Console.WriteLine("---------- Find Frequency of each word in HashTable 1 ---------- \n");
+            foreach (var word in UC1Array)
+                hTable1.CountOccurance(word);
+
+            Console.WriteLine("\n ---------- Find Frequency of each word in HashTable 2 ---------- \n");
+            foreach (var word in UC2Array)
+                hTable2.CountOccurance(word);
+
+            Console.WriteLine("\n ---------- Delete 'Avoidable' from HashTable 2 ---------- \n");
+            hTable2.Delete("avoidable");
+            hTable2.CountOccurance("avoidable");
+            hTable2.Print();
+
+            Console.WriteLine("\n----------------------------Binary Search Tree---------------------------------------");
+
+            var BST = new BinarySearchTree();
+            BST.Add(56);
+            BST.Add(30);
+            BST.Add(70);
+            BST.Add(22);
+            BST.Add(40);
+            BST.Add(11);
+            BST.Add(16);
+            BST.Add(3);
+            BST.Add(60);
+            BST.Add(95);
+            BST.Add(65);
+            BST.Add(63);
+            BST.Add(67);
+
+            Console.WriteLine(BST);
+
+            Console.WriteLine("\n Search 63 and 111 in the Tree");
+
+            BST.Search(63);
+            BST.Search(111);
+        }
+    }
+}
